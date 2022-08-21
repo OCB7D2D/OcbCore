91b420c5b67e3d83606fb29c0ccb7bf81249a566
Marcel Greter <marcel.greter@ocbnet.ch>
2022-08-21 02:43:07 +0200

    Add additional demo to show case decoupling

---
+++ Demo/3ModBaz/00-CompileModule.bat
@@ -0,0 +1,8 @@
+@echo off
+
+call MC7D2D ModBaz.dll ^
+  /reference:"%PATH_7D2D_MANAGED%\Assembly-CSharp.dll" ^
+  Harmony\*.cs ..\..\Library\HarmonyCondition.cs && ^
+echo Successfully compiled ModBaz.dll
+
+pause
---
+++ Demo/3ModBaz/Harmony/ModBaz.cs
@@ -0,0 +1,25 @@
+using System.Reflection;
+using HarmonyLib;
+using OCBNET;
+
+public class ModBaz : IModApi
+{
+    public void InitMod(Mod mod)
+    {
+        Log.Out(" Loading Patch: " + GetType().ToString());
+        var harmony = new Harmony(GetType().ToString());
+        HarmonyCondition.PatchAll(harmony, Assembly.GetExecutingAssembly());
+    }
+
+    [HarmonyCondition("HasConfig(SCore;Fire),HasConfig(SCore;Base)")]
+    [HarmonyPatch(typeof(EntityPlayerLocal))]
+    [HarmonyPatch("Update")]
+    public class EntityPlayerLocalUpdate
+    {
+        static void Prefix()
+        {
+            Log.Out("Update Inside Baz");
+        }
+    }
+
+}
